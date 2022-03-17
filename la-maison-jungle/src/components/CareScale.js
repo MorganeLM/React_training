8a04b8e225ada47c8a6e64ef9774d8f9bb25c68e
MorganeLM <morganelemarquer@lilo.org>
2022-03-17 16:07:59 +0100

    add alert for care click

--- la-maison-jungle/src/components/CareScale.js
+++ la-maison-jungle/src/components/CareScale.js
@@ -4,8 +4,22 @@ function CareScale(props) {
     const range = [1, 2, 3]
     const scaleType = type === 'light' ? '☀️' : '💧'
 
-    return (
-        <div>
+    function handleClick(type, value){
+        const quantity = {
+            1: 'peu',
+            2: 'modérement',
+            3: 'beaucoup'
+        }
+        const needType = {
+            'light': 'de lumière',
+            'water': 'd\'arrosage'
+        }
+        const message = `Cette plante requiert ${quantity[value]} ${needType[type]}`;
+        alert(message)
+    }
+
+return (
+    <div onClick={() => handleClick(props.type, scaleValue)}>
             { props.type === 'light' && 'Lumière : ' }
             { props.type === 'water' && 'Arrosage : ' }
             {range.map((rangeElem) => scaleValue >= rangeElem ? <span key={rangeElem.toString()}>{scaleType}</span> : null
